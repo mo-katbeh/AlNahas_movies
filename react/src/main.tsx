d2ad43b233c49cd2e8f22df9b8f6f555bbe60c05
mo-katbeh <hamode.katbeh@gmail.com>
2025-07-23 14:04:08 +0300

    trpc

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { trpc } from "../utils/trpc";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3000/trpc", // adjust to your backend
+    }),
+  ],
+});
+
+function App() {
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {/* Your App */}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
+export default App;
