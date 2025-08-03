efbacddf1b82f97b48d2a27828c5b4a92d9f4c4b
mo-katbeh <hamode.katbeh@gmail.com>
2025-08-03 15:18:04 +0300

    add movies

--- react/src/components/User.tsx
+++ react/src/components/User.tsx
@@ -1,22 +1,42 @@
-import React from "react";
 import { trpc } from "../../utils/trpc";
 
-const User = () => {
-  const { data, isLoading, error } = trpc.user.getAll.useQuery();
+// const User = () => {
+//   const { data, isLoading, error } = trpc.user.getAll.useQuery();
+//   if (isLoading) return <p>Loading...</p>;
+//   if (error) return <p>{error.message} </p>;
+//   if (!data || data.length === 0) return <p>No users found.</p>;
+//   // console.log("Fetched users:", data);
+//   return (
+//     <div>
+//       <h1>Users</h1>
+//       <ul>
+//         {data?.map((item) => (
+//           <li key={item.id}>{item.email} </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+const Movie = () => {
+  const { data, isLoading, error } = trpc.watchlist.getAll.useQuery();
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message} </p>;
-  if (!data || data.length === 0) return <p>No users found.</p>;
+  if (!data || data.length === 0) return <p>No Movies found.</p>;
   // console.log("Fetched users:", data);
   return (
     <div>
-      <h1>Users</h1>
+      <h1>Movies</h1>
       <ul>
         {data?.map((item) => (
-          <li key={item.id}>{item.email} </li>
+          <li key={item.id}>
+            {item.title}
+            {item.year}{" "}
+          </li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default User;
+export default Movie;
--- react/utils/trpc.ts
+++ react/utils/trpc.ts
@@ -1,4 +1,4 @@
 import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../../server/src/trpc/mainRouter';
+import type { AppRouter } from '../../server/src/trpc/routers/mainRouter';
 
 export const trpc = createTRPCReact<AppRouter>();
