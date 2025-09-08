3813f5a8958ec2ac8e7080d48a798c226b971c13
mo-katbeh <hamode.katbeh@gmail.com>
2025-09-08 16:35:55 +0300

    add icons

--- react/src/components/NavigationMenuMovies.tsx
+++ react/src/components/NavigationMenuMovies.tsx
@@ -10,6 +10,10 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Button } from "./ui/button";
 import theaterImage from "../assets/theater.jpg";
+import { IoSearchOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineNotificationsNone } from "react-icons/md";
+import { TfiMoreAlt } from "react-icons/tfi";
 export function NavigationMenuMovies() {
   return (
     <>
@@ -17,6 +21,12 @@ export function NavigationMenuMovies() {
         className="min-h-screen bg-cover bg-center overflow-y-hidden"
         style={{ backgroundImage: `url(${theaterImage})` }}
       >
+        <div className="flex flex-row-reverse m-4 gap-2">
+          <TfiMoreAlt color="white" size="12" />
+          <CgProfile color="white" size="12" />
+          <MdOutlineNotificationsNone color="white" size="12" />
+          <IoSearchOutline color="white" size="12" />
+        </div>
         <div className="flex flex-row justify-center gap-2 font-medium bg-fixed">
           <Button variant="move_nav">Home</Button>
           <Button variant="move_nav">Series</Button>
