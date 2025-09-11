6d2cb14ae26569a26b32d42c68eb54d31dde7ebd
mo-katbeh <hamode.katbeh@gmail.com>
2025-09-11 10:54:49 +0300

    update movie component

--- react/src/components/Movie.tsx
+++ react/src/components/Movie.tsx
@@ -1,6 +1,7 @@
 import { trpc } from "../../utils/trpc";
+import { Button } from "./ui/button";
 import { ScrollArea, ScrollBar } from "./ui/scroll-area";
-
+import { FaChevronRight } from "react-icons/fa6";
 const Movie = () => {
   // const{data: movies, } = trpc.movie.infinitMovies.useInfiniteQuery(
   //   {limit: 10},
@@ -14,7 +15,7 @@ const Movie = () => {
     hasNextPage,
     isFetchingNextPage,
   } = trpc.movie.infiniteMovies.useInfiniteQuery(
-    { limit: 15 },
+    { limit: 10 },
     {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     }
@@ -22,16 +23,16 @@ const Movie = () => {
   if (error) return <p> {error.message} </p>;
   if (isLoading) return <p> Loading... </p>;
   return (
-    <ScrollArea className=" rounded-md ">
-      <div className="flex w-max space-x-8 p-4">
+    <ScrollArea>
+      <div className="flex w-max space-x-6 p-14">
         {movies?.pages.map((page) =>
           page.movies.map((movie) => (
             <figure key={movie.id} className="shrink-0">
-              <div className="w-[250px] h-[300px] overflow-hidden rounded-2xl">
+              <div className="w-[250px] h-[300px] overflow-x-hidden transition duration-300 delay-150 ease-in-out hover:scale-130 p-2">
                 <img
                   src={movie.poster_url}
                   alt={movie.title}
-                  className="w-full h-full "
+                  className="w-full h-full rounded-2xl"
                 />
               </div>
             </figure>
@@ -39,36 +40,24 @@ const Movie = () => {
         )}
         <div />
         <div className="flex justify-center mt-4"></div>
-        <button
+        <Button
           onClick={() => fetchNextPage()}
           disabled={!hasNextPage || isFetchingNextPage}
-          className="px-4 py-2 rounded-xl bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
+          className=" w-[40px] h-[300px] rounded-xl mask-l-from-red-800 hover:bg-black "
         >
-          {isFetchingNextPage
-            ? "Loading more..."
-            : hasNextPage
-            ? "Load More"
-            : "No more movies"}
-        </button>
+          {isFetchingNextPage ? (
+            "Loading more..."
+          ) : hasNextPage ? (
+            <FaChevronRight />
+          ) : (
+            "No more movies"
+          )}
+        </Button>
       </div>
       <ScrollBar orientation="horizontal" />
+      <div></div>
     </ScrollArea>
   );
-  // return (
-  //   <div className="flex flex-row gap-7 p-0.5 overflow-x-scroll overflow-hidden">
-  //     {movies?.map((movie) => (
-  //       //<li key={movie.id}>
-  //       <img
-  //         src={movie.poster_url}
-  //         alt={movie.title}
-  //         className="w-[200px] h-[300px] object-cover"
-  //       />
-  //       //</li>
-  //     ))}
-  //     <button onClick={() => setPage(page - 1)}>Privous</button>
-  //     <button onClick={() => setPage(page + 1)}>Next</button>
-  //   </div>
-  // );
 };
 
 export default Movie;
