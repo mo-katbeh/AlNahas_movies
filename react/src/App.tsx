fcbce7542f5241970e86dc6b0b6d012dff7968b8
mo-katbeh <hamode.katbeh@gmail.com>
2025-09-12 09:32:38 +0300

    add form

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <>
       {/* <NavigationMenuMovies /> */}
       <UserProfileForm />
+      {/* <EmailForm /> */}
     </>
   );
 }
--- react/src/components/Form/UserProfileForm.tsx
+++ react/src/components/Form/UserProfileForm.tsx
@@ -1,5 +1,47 @@
+import { useRef, type FormEvent } from "react";
+import { Button } from "../ui/button";
+import { trpc } from "../../../utils/trpc";
+import { useForm, type FieldValues } from "react-hook-form";
+
 const UserProfileForm = () => {
-  return <div>UserProfileForm</div>;
+  const { register, handleSubmit } = useForm();
+
+  const userProfile = trpc.userProfile.createUserProfile.useMutation();
+  //   const firstNameRef = useRef<HTMLInputElement>(null);
+  const onSubmit = (data: FieldValues) => console.log(data);
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-items m-4 gap-4 border-4 border-red-800"
+      >
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          {...register("firstName")}
+          id="firstName"
+          type="text"
+          //   name="firstNameInput"
+        />
+        <label>
+          Last Name: <input name="lastNameInput" />
+        </label>
+        <label htmlFor="">
+          Birth Date: <input type="date" name="birthDateInput" />
+        </label>
+        <label htmlFor="">
+          Gender:
+          <select name="selectGender">
+            <option value="male">Male</option>
+            <option value="famle">Famale</option>
+          </select>
+        </label>
+        <label htmlFor="">
+          Phone Number: <input type="number" name="phoneNumberInput" />
+        </label>
+        <Button type="submit">Submit</Button>
+      </form>
+    </>
+  );
 };
 
 export default UserProfileForm;
