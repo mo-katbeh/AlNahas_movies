9aceb2fed00720b35fabfd4a494ceda4b45f4c55
mo-katbeh <hamode.katbeh@gmail.com>
2025-10-01 10:40:39 +0300

    add video

--- react/src/components/Home.tsx
+++ react/src/components/Home.tsx
@@ -1,5 +1,21 @@
+import { VideoPlayer, VideoPlayerContent } from "./ui/shadcn-io/video-player";
+import thriller from "../assets/videos/thriller.mp4";
 export const Home = () => {
-  return <div className="border p"></div>;
+  return (
+    // <div className="m-4 border borrder-solid  w-full h-[250px]">
+    <VideoPlayer className="m-12  overflow-hidden rounded-xl border">
+      <VideoPlayerContent
+        crossOrigin=""
+        muted
+        preload="auto"
+        autoPlay
+        playsInline
+        slot="media"
+        src={thriller}
+      />
+    </VideoPlayer>
+    // </div>
+  );
 };
 
 export default Home;
