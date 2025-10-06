60f6f8888a365bf26849393a3d53ef10673d3811
mo-katbeh <hamode.katbeh@gmail.com>
2025-10-06 21:22:32 +0300

    up

--- react/src/components/WatchListItem.tsx
+++ react/src/components/WatchListItem.tsx
@@ -6,14 +6,12 @@ import { BackgroundGradient } from "./ui/shadcn-io/background-gradient";
 const WatchListItem = () => {
   const { data } = trpc.movie.getMovies.useQuery();
   return (
-    <div className="m-2">
-      <div className="mb-2">
-        <p className="text-2xl">My Watchlist</p>
-      </div>
-      <div className="grid grid-cols-4 gap-8">
+    <div className="m-2 ">
+      <p className="text-2xl m-2 mb-4 font-bold">My Watchlist:</p>
+      <div className="relative grid grid-cols-4 gap-8">
         {data?.movies.map((movie) => (
-          <BackgroundGradient className="rounded-[22px] max-w-sm  bg-white dark:bg-zinc-900">
-            <Card className="h-[500px]">
+          <BackgroundGradient className="rounded-[22px]  bg-white dark:bg-zinc-900">
+            <Card className="h-[400px]">
               <CardContent>
                 <div className="pb-4">
                   <img
@@ -22,17 +20,22 @@ const WatchListItem = () => {
                     alt={movie.title}
                   />
                 </div>
-                <CardTitle className="text-sm">{movie.title}</CardTitle>
+                <CardTitle className="text-md">{movie.title}</CardTitle>
                 <CardDescription className="text-sm">
                   <p>{movie.release_year}</p>
 
                   <p>{movie.genre}</p>
                 </CardDescription>
 
-                <div className="flex flex-end items-center justify-between">
-                  <span className="text-sm font-bold">$199</span>
-                  <Button size="sm" className="text-xs px-2 py-1 h-7">
-                    Add
+                <div className="absolute m-2 mb-5 mr-5 right-0 bottom-0">
+                  {/* <span className="text-sm font-bold">$199</span> */}
+                  <Button
+                    size="sm"
+                    type="button"
+                    variant="destructive"
+                    className="bg-red-600/90 text-xs px-2 py-1 h-7"
+                  >
+                    Remove
                   </Button>
                 </div>
               </CardContent>
