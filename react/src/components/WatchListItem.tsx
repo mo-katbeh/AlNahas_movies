dcf089359055ba136c5210f4dcf5f19d03d4da0c
mo-katbeh <hamode.katbeh@gmail.com>
2025-10-06 15:33:39 +0300

    add movies to watchlist

--- react/src/components/WatchListItem.tsx
+++ react/src/components/WatchListItem.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { trpc } from "../../utils/trpc";
+import { Card } from "./ui/card";
 
 const WatchListItem = () => {
-  return <div>WatchListItem</div>;
+  const { data } = trpc.movie.getMovies.useQuery();
+  return (
+    <div className="m-2">
+      <div className="mb-2">
+        <p className="text-2xl">My Watchlist</p>
+      </div>
+      <div className="grid grid-cols-6 gap-4">
+        <Card>
+          {data?.movies.map((movie) => (
+            <img src={movie.poster_url} alt={movie.title} />
+          ))}
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default WatchListItem;
