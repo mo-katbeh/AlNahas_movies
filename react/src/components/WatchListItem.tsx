59654c1c3a4df0d95e2cba026362db60b7d5f23b
mo-katbeh <hamode.katbeh@gmail.com>
2025-10-07 11:25:49 +0300

    edit card in watclist page

--- react/src/components/WatchListItem.tsx
+++ react/src/components/WatchListItem.tsx
@@ -1,48 +1,65 @@
 import { trpc } from "../../utils/trpc";
+import Loader from "./loader/styled-wrapper";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardDescription, CardTitle } from "./ui/card";
 import { BackgroundGradient } from "./ui/shadcn-io/background-gradient";
 
 const WatchListItem = () => {
-  const { data } = trpc.movie.getMovies.useQuery();
+  const { data, isLoading, error } = trpc.movie.getMovies.useQuery();
+  if (error) console.log("error in Watchlist page", error);
   return (
     <div className="m-2 ">
       <p className="text-2xl m-2 mb-4 font-bold">My Watchlist:</p>
-      <div className="relative grid grid-cols-4 gap-8">
-        {data?.movies.map((movie) => (
-          <BackgroundGradient className="rounded-[22px]  bg-white dark:bg-zinc-900">
-            <Card className="h-[400px]">
-              <CardContent>
-                <div className="pb-4">
+      {!data?.movies ? (
+        <div>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <p className="text-2xl md:text-2xl font-semibold text-white line-clamp-6">
+              Nothing here yet, stay tuned 👀
+            </p>
+          )}
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-8">
+          {data?.movies.map((movie) => (
+            <BackgroundGradient className="rounded-xl bg-white dark:bg-zinc-900">
+              <Card className="relative h-[400px] py-0 gap-0">
+                <CardContent className=" px-0 w-full ">
+                  {/* <div className=""> */}
                   <img
-                    className="aspect-square rounded-md bg-gray-100"
+                    className="rounded-xl rounded-bl-none rounded-br-none h-[250px] w-full bg-gray-100"
                     src={movie.poster_url}
                     alt={movie.title}
                   />
-                </div>
-                <CardTitle className="text-md">{movie.title}</CardTitle>
-                <CardDescription className="text-sm">
-                  <p>{movie.release_year}</p>
+                  {/* </div> */}
+                  <div className="p-3">
+                    <CardTitle className="text-sm pb-5">
+                      {movie.title}
+                    </CardTitle>
+                    <CardDescription>
+                      <p>{movie.release_year}</p>
+                      <p>{movie.genre}</p>
+                    </CardDescription>
+                  </div>
 
-                  <p>{movie.genre}</p>
-                </CardDescription>
-
-                <div className="absolute m-2 mb-5 mr-5 right-0 bottom-0">
-                  {/* <span className="text-sm font-bold">$199</span> */}
-                  <Button
-                    size="sm"
-                    type="button"
-                    variant="destructive"
-                    className="bg-red-600/90 text-xs px-2 py-1 h-7"
-                  >
-                    Remove
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
-          </BackgroundGradient>
-        ))}
-      </div>
+                  <div className="absolute m-2 mb-5 mr-5 right-0 bottom-0">
+                    {/* <span className="text-sm font-bold">$199</span> */}
+                    <Button
+                      size="sm"
+                      type="button"
+                      variant="destructive"
+                      className="bg-red-600/90 text-xs px-2 py-1 h-7"
+                    >
+                      Remove
+                    </Button>
+                  </div>
+                </CardContent>
+              </Card>
+            </BackgroundGradient>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
